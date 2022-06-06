25e0d418f753da8b9b669aaebd62b117a8c29338
wangqiang91 <786528604@qq.com>
2022-06-06 21:54:32 +0800

    mysql

--- test_interface_HLJ/utils/operate_mysql.py
+++ test_interface_HLJ/utils/operate_mysql.py
@@ -2,26 +2,40 @@ import pymysql
 
 class OperateMysql():
     def __init__(self):
-        self.cursortest = self.connectTestMysql()
+        self.cursortest = self.connectTestMysql().cursor()
     def connectTestMysql(self):
-        conn = pymysql.connect(
-            host="localhost",
-            port=3306,
-            user="root",
-            password="123456",
-        )
-        cursor = conn.cursor()
-        return cursor
+        try:
+            self.conntest = pymysql.connect(
+                host="localhost",
+                port=3306,
+                user="root",
+                password="123456",
+            )
+            return self.conntest
+        except:
+            return "连接失败"
     def selectData(self,sql):
-        self.cursortest.execute(sql)
-        data_list = []
-        for i in self.cursortest.fetchall():
-            data_list.append(i)
-        return data_list
+        try:
+            self.cursortest.execute(sql)
+            data = self.cursortest.fetchall()
+            return data
+        except:
+            return "查询失败，请检查"
+        finally:
+            self.conntest.close()
+    def addData(self,sql):
+        try:
+            self.cursortest.execute(sql)
+            self.conntest.commit()
+        except:
+            self.conntest.rollback()
+        finally:
+            self.conntest.close()
 
 
 if __name__ == "__main__":
     sql = "select * from find_job.course limit 7"
+    # sql = "insert into find_job.course() values ('05','地理2','05')"
     res = OperateMysql().selectData(sql)
     print(res)
-
+    # OperateMysql().connectTestMysql().close()
