68e8884c0ff9b33b1044afcb03d0da743a0948f6
wangqiang91 <786528604@qq.com>
2023-11-15 17:07:27 +0800

    conf

--- test_interface_HLJ/utils/configuration.py
+++ test_interface_HLJ/utils/configuration.py
@@ -1,21 +1,30 @@
 import json
 import os
+import configparser
 path1 = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-path2 = os.path.join(path1,'test_case','configuration.json')
-
 
 class Configuration():
-    def __init__(self,excel_file = path2):
-        self.file = excel_file
-        self.f = open(self.file)
-        self.data = json.loads(self.f.read())
-        self.f.close()
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read(path1 + r"\test_case\test.conf",encoding="utf-8")
     def get_Excel_Name(self):
-        return self.data["fileName"]
+        return self.config.get('run_test_case_file','fileName')
     def get_Receive_Email(self):
-        emails = self.data["receiveEmail"]
-        email_list = emails.split(",")
-        return email_list
+        email_receivers = self.config.get('receiveEmail','receiveEmail')
+        mail_receivers_list = email_receivers.split(",")
+        return mail_receivers_list
+# class Configuration():
+#     def __init__(self,excel_file = path2):
+#         self.file = excel_file
+#         self.f = open(self.file)
+#         self.data = json.loads(self.f.read())
+#         self.f.close()
+#     def get_Excel_Name(self):
+#         return self.data["fileName"]
+#     def get_Receive_Email(self):
+#         emails = self.data["receiveEmail"]
+#         email_list = emails.split(",")
+#         return email_list
 
 if __name__ == '__main__':
     data = Configuration().get_Receive_Email()
